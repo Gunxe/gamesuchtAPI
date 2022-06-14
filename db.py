25bc1891357934393c5dde285debc91aa10559d9
Gunxe <l.gilomen@limo-bern.ch>
2022-06-14 18:23:00 +0200

    fix

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ database = 'gamesucht'
 username = 'slr'
 password = '#h:!~8maB;)@H#[*'
 #cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-cnxn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:gamesucht.database.windows.net,1433;Database=gamesucht;Uid=slr;Pwd={#h:!~8maB;)@H#[*};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+cnxn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server};Server=tcp:gamesucht.database.windows.net,1433;Database=gamesucht;Uid=slr;Pwd={#h:!~8maB;)@H#[*};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 cursor = cnxn.cursor()
 
 def getUmfrage(Name):
